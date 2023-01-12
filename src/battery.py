b16d8ec950ac4a52e23fce92f16380fb4d42ed76
dhinogz <davidhinojosagzz@gmail.com>
2023-01-11 18:19:07 -0600

    Add current_date attribute to comply with given UML design

--- src/battery.py
+++ src/battery.py
@@ -12,13 +12,14 @@ class Spindler(Battery):
     def __init__(self, last_service_date: int) -> None:
         self.last_service_date = last_service_date
         self.check_time = 2 # years
+        self.current_date = datetime.today().date()
 
 
     def needs_service(self):
         service_threshold_date = self.last_service_date.replace(
             year=self.last_service_date.year + self.check_time
         )
-        return service_threshold_date < datetime.today().date()
+        return service_threshold_date < self.current_date
 
 
 
@@ -27,10 +28,12 @@ class Nubbin(Battery):
     def __init__(self, last_service_date: int) -> None:
         self.last_service_date = last_service_date
         self.check_time = 4 # years
+        self.current_date = datetime.today().date()
+
 
     def needs_service(self):
         service_threshold_date = self.last_service_date.replace(
             year=self.last_service_date.year + self.check_time
         )
-        return service_threshold_date < datetime.today().date()
+        return service_threshold_date < self.current_date
             
